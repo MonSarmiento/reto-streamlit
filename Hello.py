698b4b1c30136275e4e9df036ce468335697ed2f
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 16:06:41 -0600

    Hello.py

--- Hello.py
+++ Hello.py
@@ -20,11 +20,11 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Hello esta es una prueba",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# pagina de monica! 👋")
 
     st.sidebar.success("Select a demo above.")
 
