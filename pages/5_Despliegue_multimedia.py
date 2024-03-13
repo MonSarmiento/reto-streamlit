fd0861061268235738c8033affc834822f43b653
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 17:57:04 -0600

    5_Despliegue_multimedia.py

--- pages/5_Despliegue_multimedia.py
+++ pages/5_Despliegue_multimedia.py
@@ -1,7 +1,7 @@
 from PIL import Image
 def run():
   st.header('st.dataframe')
-  image = Image.open('https://github.com/MonSarmiento/reto-streamlit/blob/main/oso_panda.jpg')
+  image = Image.open("https://github.com/MonSarmiento/reto-streamlit/blob/main/oso_panda.jpg")
   st.image(image, caption='Oso panda en la montaña')
   if __name__ == "__main__":
     run()
