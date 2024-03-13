95049e4207236fcba27447172f560ed3ce126ded
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 17:05:59 -0600

    Pandas_Styler.py

--- pages/Pandas_Styler.py
+++ pages/Pandas_Styler.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+def run():
+  dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+  st.dataframe(dataframe.style.highlight_max(axis=0))
+if __name__ == "__main__":
+    run()
