9eaf46b57260dfaf1698f0b46bb951e915612160
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 17:12:13 -0600

    4_Despliegue.py

--- pages/4_Despliegue.py
+++ pages/4_Despliegue.py
@@ -1,12 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit.logger import get_logger
-LOGGER = get_logger(__name__)
 
+#ST.DATAFRAME
 def run():
   dataframe = np.random.randn(10, 20)
   st.dataframe(dataframe)
 if __name__ == "__main__":
     run()
-  
+
+#PANDAS STYLER
+import streamlit as st
+import pandas as pd
+import numpy as np
+def run():
+  dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+  st.dataframe(dataframe.style.highlight_max(axis=0))
+if __name__ == "__main__":
+    run()
+
+#ST.TABLE
+def run():
+  dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+  st.table(dataframe)
+if __name__ == "__main__":
+    run()
