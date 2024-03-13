34ffeed81bb6360183831e066ec583ec4d346f95
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 16:59:24 -0600

    4_Despliegue.py

--- pages/4_Despliegue.py
+++ pages/4_Despliegue.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+from streamlit.logger import get_logger
+LOGGER = get_logger(__name__)
+
+def run():
+  dataframe = np.random.randn(10, 20)
+  st.dataframe(dataframe)
+  
