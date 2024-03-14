55c3f85797b9da0f6a9bb31ae036cc76383cecc1
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 18:14:57 -0600

    Update 5_Despliegue_multimedia.py

--- pages/5_Despliegue_multimedia.py
+++ pages/5_Despliegue_multimedia.py
@@ -6,6 +6,12 @@ def run():
   st.header('st.image')
   image = Image.open('/mount/src/reto-streamlit/pages/oso_panda.jpg')
   st.image(image, caption='Oso panda en la montaña')
+  #funcion para animacion
+  def load_lottieurl(url):
+  r=requests.get(url)
+  if r.status_code != 200 :
+    return None
+  return r.jason()
   lottie_coding = load_lottieurl("https://lottie.host/075ab19b-23cd-45a8-9980-4f3004626673/1vnfbbKSQx.json")
 if __name__ == "__main__":
   run()
