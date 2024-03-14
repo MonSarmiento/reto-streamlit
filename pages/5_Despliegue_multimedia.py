962b1f998ed9f686939f556240daac4b4b09ac30
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 18:22:31 -0600

    Update 5_Despliegue_multimedia.py

--- pages/5_Despliegue_multimedia.py
+++ pages/5_Despliegue_multimedia.py
@@ -1,7 +1,7 @@
 #IMAGEN JPG
 import streamlit as st
 from PIL import Image
-from streamlit_lottie import st_lottie
+
 def run():
   st.header('st.image')
   image = Image.open('/mount/src/reto-streamlit/pages/oso_panda.jpg')
@@ -9,11 +9,5 @@ def run():
   #funcion para animacion
 if __name__ == "__main__":
   run()
-def load_lottieurl(url):
-  r=requests.get(url)
-  if r.status_code != 200 :
-    return None
-  return r.jason()
-lottie_coding = load_lottieurl("https://lottie.host/075ab19b-23cd-45a8-9980-4f3004626673/1vnfbbKSQx.json")
 
 #AUDIO
