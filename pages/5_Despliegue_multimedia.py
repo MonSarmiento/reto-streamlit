f3bff11c57192a3f647410e0e2925ddfec5d7885
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 18:06:03 -0600

    Update 5_Despliegue_multimedia.py

--- pages/5_Despliegue_multimedia.py
+++ pages/5_Despliegue_multimedia.py
@@ -1,7 +1,8 @@
 def run():
   from PIL import Image
-  st.header('st.dataframe')
+
   image = Image.open('oso_panda.jpg')
   st.image(image, caption='Oso panda en la montaña')
 if __name__ == "__main__":
   run()
+#st.header('st.dataframe')
