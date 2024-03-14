9f993ad2e9caa67aa5b41c08e3d2f91694520cfb
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 18:13:56 -0600

    5_Despliegue_multimedia.py

--- pages/5_Despliegue_multimedia.py
+++ pages/5_Despliegue_multimedia.py
@@ -1,8 +1,14 @@
+#IMAGEN JPG
 import streamlit as st
 from PIL import Image
+from streamlit_lottie import st_lottie
 def run():
+  st.header('st.image')
   image = Image.open('/mount/src/reto-streamlit/pages/oso_panda.jpg')
   st.image(image, caption='Oso panda en la montaña')
+  lottie_coding = load_lottieurl("https://lottie.host/075ab19b-23cd-45a8-9980-4f3004626673/1vnfbbKSQx.json")
 if __name__ == "__main__":
   run()
-#st.header('st.dataframe')
+
+
+#AUDIO
