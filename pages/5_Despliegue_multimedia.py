81aeeb8cca2710ff7788558f3eb02ef022ac62d5
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-13 18:09:01 -0600

    Update 5_Despliegue_multimedia.py

--- pages/5_Despliegue_multimedia.py
+++ pages/5_Despliegue_multimedia.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from PIL import Image
-def run():
-
-
+#def run():
   image = Image.open('oso_panda.jpg')
   st.image(image, caption='Oso panda en la montaña')
-if __name__ == "__main__":
+#if __name__ == "__main__":
   run()
 #st.header('st.dataframe')
