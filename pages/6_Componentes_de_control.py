bfc759f92d296b40302d498f4b4dadcc1d90eedd
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-15 12:44:52 -0600

    Update 6_Componentes_de_control.py

--- pages/6_Componentes_de_control.py
+++ pages/6_Componentes_de_control.py
@@ -1,4 +1,4 @@
 #RADIO BOTTOMS
-
-selected_class = st.radio("Select Class", titanic_data['class'].unique())
-st.write("Select Class:",selected_class)
+def run():
+  selected_class = st.radio("Select Class", titanic_data['class'].unique())
+  st.write("Select Class:",selected_class)
