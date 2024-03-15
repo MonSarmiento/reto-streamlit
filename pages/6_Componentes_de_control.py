801c0032a2490aadde17ef55e9dc86ff977c6490
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-15 12:54:40 -0600

    Update 6_Componentes_de_control.py

--- pages/6_Componentes_de_control.py
+++ pages/6_Componentes_de_control.py
@@ -1,6 +1,9 @@
 #RADIO BOTTOMS
+import pandas as pd
+import streamlit as st
+
 def run():
-  titanic_data=text.open('pages/titanic.csv')
+  titanic_data=pd.read_csv('pages/titanic.csv')
   selected_class = st.radio("Select Class", titanic_data['class'].unique())
   st.write("Select Class:",selected_class)
 if __name__ == "__main__":
