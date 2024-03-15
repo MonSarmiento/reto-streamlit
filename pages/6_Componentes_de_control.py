2679bc7ee626e85cb47b4e2da380da6a0c4b6e3c
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-15 12:59:30 -0600

    Update 6_Componentes_de_control.py

--- pages/6_Componentes_de_control.py
+++ pages/6_Componentes_de_control.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def run():
   titanic_data=pd.read_csv('/mount/src/reto-streamlit/pages/titanic.csv')
-  selected_class = sidebar.radio("Select Class", titanic_data['class'].unique())
+  selected_class = st.radio("Select Class", titanic_data['class'].unique())
   st.write("Select Class:",selected_class)
 if __name__ == "__main__":
   run()
