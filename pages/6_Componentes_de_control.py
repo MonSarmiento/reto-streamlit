2527cabfb225cff9cb6018840729b84d7469a0e3
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 16:33:57 -0600

    Update 6_Componentes_de_control.py

--- pages/6_Componentes_de_control.py
+++ pages/6_Componentes_de_control.py
@@ -9,4 +9,7 @@ st.write("Select Class:",selected_class)
 
 # SELECT BOX
 selected_sex = st.selectbox("Select Sex", titanic_data['Sex'].unique())
+#st.write para desplegar la casilla de verificación
+# la letra “f”, lo cual indica que se capturará información
+# se pone el signo de admiración y la letra “r” para indicar que se escribirá la opción que se haya elegido.
 st.write(f"Selected Option: {selected_sex!r}")
