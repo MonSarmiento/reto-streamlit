90a040222f39cb6b0b4f50a069ad415806bf7e87
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 17:51:56 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -1,8 +1,23 @@
 #import streamlit as st
 from altair.utils.deprecation import deprecated
+
+@deprecated("load_dataset is deprecated. " "Use the vega_datasets package instead.")
+def load_dataset(name):
+    """Load a dataset by name as a pandas.DataFrame."""
+    import vega_datasets
+
+    return vega_datasets.data(name)
+
+
+@deprecated("load_dataset is deprecated. " "Use the vega_datasets package instead.")
+def list_datasets():
+    """List the available datasets."""
+    import vega_datasets
+
+    return vega_datasets.data.list_datasets()
 #ALTAIR
-from vega_datasets import datasets as vega_data
-import pandas as pd
-movie_df = pd.read_json(vega_data.movies.url)
+#from vega_datasets import datasets as vega_data
+#import pandas as pd
+#movie_df = pd.read_json(vega_data.movies.url)
 
 
