306057dfd3ea6fcadf2465d17b60ade5087189cf
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 18:40:55 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -7,9 +7,9 @@ def run():
   titanic_link = 'https://raw.githubusercontent.com/MonSarmiento/reto-streamlit/main/pages/titanic.csv'
   titanic_data = pd.read_csv(titanic_link)
   df_edad = titanic_data[titanic_data["Age"] >= 30]
-  #df_edad.shape
+  df_edad.shape
   alt.Chart(titanic_data)
-  #alt.Chart(df_edad).mark_point()
+  alt.Chart(df_edad).mark_bar()
 
 if __name__ == "__main__":
     run()
