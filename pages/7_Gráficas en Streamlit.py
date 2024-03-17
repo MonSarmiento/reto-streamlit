27d6aad84df421bc89961047bc2e7bf8cdaf1384
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 18:32:28 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -1,7 +1,8 @@
 import altair as alt
 import pandas as pd
-
+import streamlit as st
 #ALTAIR
+#def run():
 titanic_link = 'https://raw.githubusercontent.com/MonSarmiento/reto-streamlit/main/pages/titanic.csv'
 titanic_data = pd.read_csv(titanic_link)
 df_edad = titanic_data[titanic_data["Age"] >= 30]
