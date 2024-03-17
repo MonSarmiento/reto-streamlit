24b9133051e040ee5efb386aee48166214e10957
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-17 14:40:51 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -129,6 +129,12 @@ hover.tooltips=[
 ]
 p.add_tools(hover) 
 
-
-
 bokeh.io.show(p)
+
+from bokeh.plotting import figure, show
+
+p = figure(width=400, height=400)
+p.hbar(y=[1, 2, 3], height=0.5, left=0,
+       right=[1.2, 2.5, 3.7], color="navy")
+
+show(p)
