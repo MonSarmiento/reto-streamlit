38616fd232d674c559213753c9b12c5e00fe0479
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 20:25:03 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -1,7 +1,7 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
-
+import numpy as np
 
 #ALTAIR
 
@@ -11,6 +11,9 @@ movie_df = pd.read_json(movies_link)
 #Con la siguiente instrucción, podrás ver que movie_df es en realidad un dataframe de Pandas que tiene 3201 películas (filas) con 16 características de información (columnas) cada una
 movie_df.shape
 
+def run():
 #ejecutando la siguiente  instrucción se podrán visualizar las primeras 5 filas del dataframe
-movie_df.head(5)
+  movie_df.head(5)
+if __name__ == "__main__":
+    run()
 
