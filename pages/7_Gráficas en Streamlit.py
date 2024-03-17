7694beb1e6ed41469e910504083d1e2bc5ee3ef1
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 18:44:35 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -1,15 +1,14 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+
 #ALTAIR
 
-def run():
-  titanic_link = 'https://raw.githubusercontent.com/MonSarmiento/reto-streamlit/main/pages/titanic.csv'
-  titanic_data = pd.read_csv(titanic_link)
-  df_edad = titanic_data[titanic_data["Age"] >= 30]
-  df_edad.shape
-  alt.Chart(titanic_data)
-  alt.Chart(df_edad).mark_bar()
-
-if __name__ == "__main__":
-    run()
+titanic_link = 'https://raw.githubusercontent.com/MonSarmiento/reto-streamlit/main/pages/titanic.csv'
+titanic_data = pd.read_csv(titanic_link)
+df_edad = titanic_data[titanic_data["Age"] >= 30]
+df_edad.shape
+alt.Chart(titanic_data).mark_point().encode(
+  alt.X('Age'),
+  alt.Y('Worldwide_Gross')
+)
