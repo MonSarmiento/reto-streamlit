fb320a5c167aabc0594870c2b3a58a42ba2be381
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-17 11:12:41 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -37,11 +37,9 @@ alt.Chart(movie_2000).mark_point().encode(
 import altair as alt
 
 # load a simple dataset as a pandas DataFrame
-from vega_datasets import data
-cars = data.cars()
 
 alt.Chart(cars).mark_point().encode(
-    x='Horsepower',
-    y='Miles_per_Gallon',
+    x='Production Budget',
+    y='Worldwide Gross',
     color='Origin',
 )
