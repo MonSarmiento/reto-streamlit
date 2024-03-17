e9a827b934ffa59a5babc6e7abcd0eb1969b93d1
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 18:23:21 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -2,9 +2,6 @@ import altair as alt
 
 
 #ALTAIR
-from vega_datasets import data 
-import data as vega_data
-import pandas as pd
-movie_df = pd.read_json(vega_data.movies.url)
-movie_df.head(5)
+DataFrame=
+alt.Chart(DataFrame).mark_point()
 
