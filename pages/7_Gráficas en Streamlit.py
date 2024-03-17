53407fd330ede94348acfa57c524dde50ee985e3
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 18:15:47 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -2,7 +2,7 @@ import altair as alt
 
 
 #ALTAIR
-from vega_datasets import datas as vega_data
+from vega_datasets import data as vega_data
 import pandas as pd
 movie_df = pd.read_json(vega_data.movies.url)
 movie_df.head(5)
