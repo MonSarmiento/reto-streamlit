42db666a97fe0d24f4d996addd27853648deabdd
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 18:24:19 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -2,6 +2,8 @@ import altair as alt
 
 
 #ALTAIR
-DataFrame=
-alt.Chart(DataFrame).mark_point()
+titanic_link = 'https://raw.githubusercontent.com/MonSarmiento/reto-streamlit/main/pages/titanic.csv'
+titanic_data = pd.read_csv(titanic_link)
+
+alt.Chart(titanic_data).mark_point()
 
