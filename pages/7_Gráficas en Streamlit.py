d0fb494f1b1ffcd71458297e06ac6b95e7a3e261
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-16 20:25:35 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -9,7 +9,7 @@ movies_link = 'https://raw.githubusercontent.com/vega/vega/main/docs/data/movies
 movie_df = pd.read_json(movies_link)
 
 #Con la siguiente instrucción, podrás ver que movie_df es en realidad un dataframe de Pandas que tiene 3201 películas (filas) con 16 características de información (columnas) cada una
-movie_df.shape
+#movie_df.shape
 
 def run():
 #ejecutando la siguiente  instrucción se podrán visualizar las primeras 5 filas del dataframe
