7d1c4f51ccb943945fb7c009c282c00105226d3b
MonSarmiento <162919758+MonSarmiento@users.noreply.github.com>
2024-03-17 12:30:58 -0600

    Update 7_Gráficas en Streamlit.py

--- pages/7_Gráficas en Streamlit.py
+++ pages/7_Gráficas en Streamlit.py
@@ -78,3 +78,18 @@ ptriangle(y, x, color='gold', size=10, legend='triangle')
 p.legend.click_policy='hide'
 show(p)
 
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+
+output_file('columndatasource_example.html')
+
+df=pd.read_csv('thor_wwii.csv')
+sample = df.sample(50)
+source = ColumnDataSource(sample)
+p = figure()
+p.circle(x='TOTAL_TONS', y='AC_ATTACKING',
+         source=source,
+         size=10, color='green')
+
